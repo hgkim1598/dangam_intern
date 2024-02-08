d722784e2b0aecc5e2161323c24fb90192234bf5
hgkim1598 <hgkim1598@gmail.com>
2024-02-08 18:00:17 +0900

    FourIdioms.vue변경-dropdown,button

--- 0208/0208second/src/components/FourIdioms.vue
+++ 0208/0208second/src/components/FourIdioms.vue
@@ -1,14 +1,34 @@
 <template>
+  <div>
     <div>
-      <b-table striped hover :items="items" :fields="fields"></b-table>
+      <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
+        <b-dropdown-item>First Action</b-dropdown-item>
+        <b-dropdown-item>Second Action</b-dropdown-item>
+        <b-dropdown-item>Third Action</b-dropdown-item>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-item active>Active action</b-dropdown-item>
+        <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+      </b-dropdown>
     </div>
-  </template>
+    <b-table striped hover :items="items" :fields="fields">
+      <template #cell(actions)="row">
+        <!-- 제어 버튼 -->
+        <div class="btn-group" role="group">
+          <b-button size="sm" variant="warning" @click="openItem(row.item)">펼침</b-button>
+          <b-button size="sm" variant="primary" @click="editItem(row.item)">수정</b-button>
+          <b-button size="sm" variant="danger" @click="deleteItem(row.item)">삭제</b-button>
+        </div>
+      </template>
+    </b-table>
+  </div>
+</template>
   
   <script>
   import axios from 'axios';
 
     export default {
       data() {
+        // let items = response.data.content.sort((a,b) => {return b.id - a.id})
         return {
           fields:[
             {
@@ -31,6 +51,14 @@
               key: 'contents_detail',
               label: '뜻풀이'
             },
+            {
+              key: 'actions',
+              label: '제어',
+              class: 'text-center',
+              thClass: 'text-center',
+              sortable: false,
+              formatter: this.formatActions,
+            },
           ],
           items: []
         };
@@ -43,11 +71,31 @@
         axios.get(apiUrl)
           .then(response => {
           // 받은 데이터를 Vue 데이터에 저장
-          this.items = response.data;
+          this.items = response.data.content;
         })
         .catch(error => {
           console.error('데이터를 불러오는 중 오류 발생:', error);
         });
+      },
+      methods: {
+        formatActions(value, key, item){
+          return `
+            <div class="btn-group" role="group">
+              <b-button size="sm" variant="primary" @click="openItem(item)">펼침</b-button>
+              <b-button size="sm" variant="primary" @click="editItem(item)">수정</b-button>
+              <b-button size="sm" variant="danger" @click="deleteItem(item)">삭제</b-button>
+            </div>
+          `;
+        },
+        editItem(item) {
+          // 펼침 버튼을 누를 때 수행할 동작 정의
+        },
+        editItem(item) {
+          // 수정 버튼을 누를 때 수행할 동작 정의
+        },
+        deleteItem(item) {
+          // 삭제 버튼을 누를 때 수행할 동작 정의
+        },
       }
     }
   </script>
